eada69bc4d84b47dd4064f29a7f0b2e428da07c3
Herbalcode <adeboyahmad@gmail.com>
2022-12-05 16:44:42 +0100

    leaning how to import module

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# simple add script
+# import module add_0
+# Author:Herbalcode
+
+
+if __name__ == "__main__":
+
+    from add_0 import add
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
