55b659c28783c1ef8201164c404f83f66425d507
Herbalcode <adeboyahmad@gmail.com>
2022-12-06 06:46:50 +0100

    import module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -7,8 +7,7 @@
 
 if __name__ == "__main__":
 
-    from add_0 import add
+    from add_0 import add 
     a = 1
     b = 2
-    result = add(a, b)
-    print("{} + {} = {}".format(a, b, result))
+    print("{} + {} = {}".format(a, b,(add(a, b))))
